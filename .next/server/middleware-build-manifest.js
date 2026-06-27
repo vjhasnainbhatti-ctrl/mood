f750701c6a82b385ac9cade0675f4bb150494375
vjhasnainbhatti-ctrl <vj.hasnainbhatti@gmail.com>
2026-06-27 09:38:54 +0500

    Updated trace

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -9,11 +9,6 @@ self.__BUILD_MANIFEST = {
   "lowPriorityFiles": [],
   "rootMainFiles": [],
   "pages": {
-    "/": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/index.js"
-    ],
     "/_app": [
       "static/chunks/webpack.js",
       "static/chunks/main.js",
@@ -28,6 +23,11 @@ self.__BUILD_MANIFEST = {
       "static/chunks/webpack.js",
       "static/chunks/main.js",
       "static/chunks/pages/dashboard.js"
+    ],
+    "/dashboard/achievements": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/dashboard/achievements.js"
     ],
     "/dashboard/stats": [
       "static/chunks/webpack.js",
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ef69f3f084fb1b95"; }
+/******/ 		__webpack_require__.h = function() { return "484a5a3da93e472b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
